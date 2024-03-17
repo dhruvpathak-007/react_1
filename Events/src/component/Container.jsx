60f7660e21cfd0319a72d12930b08c65fff8f5ec
dhruvpathak-007 <dhruvpathak74@gmail.com>
2024-03-17 10:49:05 +0530

    Reseting branch

--- Events/src/component/Container.jsx
+++
@@ -1,7 +0,0 @@
-/* eslint-disable react/prop-types */
-import styles from "./Container.module.css";
-const Container = ({ children }) => {
-  return <div className={styles.container}>{children}</div>;
-};
-
-export default Container;
--- Events/src/component/ErrorMessage.jsx
+++
@@ -1,6 +0,0 @@
-/* eslint-disable react/prop-types */
-function ErrorMessage({ items }) {
-  return items.length === 0 ? <h1>I m hungry</h1> : null;
-}
-
-export default ErrorMessage;
